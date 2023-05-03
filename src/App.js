2c4271cd2d014b4a2f47a89baf79a2ced204052d
sharif24 <azizur15-2124@diu.edu.bd>
2023-05-03 11:40:30 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Countries from "./componeents/Countries";
+import "./App.css";
+const url = "https://restcountries.com/v3.1/all";
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setIsError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [filtercountries, setFilterCountries] = useState(countries);
+  const featchData = async (url) => {
+    setIsLoading(true);
 
-function App() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsLoading(false);
+      setFilterCountries(data);
+      setIsError(null);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(error);
+    }
+  };
+  useEffect(() => {
+    featchData(url);
+  }, []);
+
+  const handelRemoveCountry = (name) => {
+    const filter = filtercountries.filter(
+      (country) => country.name.common !== name
+    );
+    setFilterCountries(filter);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Country App</h1>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error.message}</h2>}
+      {countries && (
+        <Countries
+          countries={filtercountries}
+          onRemoveCountry={handelRemoveCountry}
+        />
+      )}
+    </>
   );
-}
+};
 
 export default App;
